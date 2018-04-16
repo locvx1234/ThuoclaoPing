44e13eacd59168380592b875d2ff4dc63db96e51
Loc VU <locvx1234@gmail.com>
2018-04-16 17:43:15 +0700

    update README

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # ThuoclaoPing
+
+
+ 
+Environment 
+-----------
+python3.5
+pip9.0.2
+
+
+Install
+-------
+
+Use Virtualenv [Options]
+
+```
+$ virtualenv -p python3 env
+$ source env/bin/activate
+```
+
+Clone and install dependences :
+
+```
+(env)$ git clone https://github.com/locvx1234/ThuoclaoPing
+(env)$ cd ThuoclaoPing/thuoclao
+(env)$ pip install -r requirements.txt
+```
+
+Create databate : 
+
+```
+(env)$ python manage.py makemigratetions
+(env)$ python manage.py migrate
+```
+
+Create superuser:
+
+```
+(env)$ python manage.py createsuperuser
+```
+
+Runserver: 
+
+```
+(env)$ python manage.py runserver 
+```
+
+
+
+
--- thuoclao/check/views.py
+++ thuoclao/check/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
 
 from .models import Host, Service
 
@@ -8,13 +9,13 @@ from .models import Host, Service
 def index(request):
     if request.user.is_authenticated:
         context = {}
-        hosts = Host.objects.all()
+        user_id = User.objects.get(username=request.user.username).id
+        hosts = Host.objects.filter(user_id=user_id)
         services = Service.objects.all()
         context['hosts'] = hosts
         context['services'] = services
         context['count_host'] = len(hosts)
         context['count_service'] = len(services)
-
         return render(request, 'check/index.html', context)
     else:
         return HttpResponseRedirect('/accounts/login')
@@ -22,9 +23,6 @@ def index(request):
 
 def view_html(request):
     context = {}
-    # The template to be loaded as per gentelella.
-    # All resource paths for gentelella end in .html.
-
     # Pick out the html file name from the url. And load that template.
     load_template = request.path.split('/')[-1]
     template = loader.get_template('./' + load_template)
@@ -33,7 +31,8 @@ def view_html(request):
 
 def host(request):
     context = {}
-    hosts = Host.objects.all()
+    user_id = User.objects.get(username=request.user.username).id
+    hosts = Host.objects.filter(user_id=user_id)
     context['hosts'] = hosts
     return render(request, 'check/host.html', context)
 
