dfae96db3065b2e5f7ce730e3f97748a0a84feea
Loc VU <locvx1234@gmail.com>
2018-04-19 17:47:12 +0700

    add host ok

--- thuoclao/check/views.py
+++ thuoclao/check/views.py
@@ -35,12 +35,27 @@ def host(request):
     context = {}
     user = User.objects.get(username=request.user.username)
     hosts = Host.objects.filter(user_id=user.id)
+    services = Service.objects.filter(host__in=hosts).distinct()
+
+    list_service = [service.service_name for service in services]   # ex ['HTTP', 'PING']
+    context['list_service'] = list_service
     context['hosts'] = hosts
     if request.method == 'POST':
         hostname = request.POST.get('hostname')
         ip_address = request.POST.get('ip-host')
         host_data = Host(hostname=hostname, ip_address=ip_address, user=user)
         host_data.save()
+
+        check = request.POST.getlist('checks[]')  # checklist: HTTP, Ping
+
+        for item in check:
+            if item.upper() in list_service:  # service existed
+                for service in services.filter(service_name=item.upper()):
+                    service.host.add(host_data.id)
+            else:  # service does not exist
+                service_data = Service(service_name=item.upper(), ok=0, warning=0, critical=0, interval_check=0)
+                service_data.save()
+                service_data.host.add(host_data)
         return HttpResponseRedirect(reverse('host'))
     return render(request, 'check/host.html', context)
 
@@ -54,10 +69,17 @@ def delete_host(request, host_id):
 def edit_host(request, host_id):
     host_data = Host.objects.filter(id=host_id)
     user = User.objects.get(username=request.user.username)
+    # services = Service.objects.filter(host__in=hosts).distinct()
+
     if request.method == 'POST':
         hostname = request.POST.get('hostname')
         ip_address = request.POST.get('ip-host')
         host_data.update(hostname=hostname, ip_address=ip_address, user=user)
+        check = request.POST.getlist('checks[]')
+        # for item in check:
+        #     for service in services:
+        #         if service.service_name.lower() == item:
+        #             service.host.add(host_data.id)
     return HttpResponseRedirect(reverse('host'))
 
 
