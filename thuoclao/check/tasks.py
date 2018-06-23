9b45f46aef0eca8dc7567b70380fec99082e9713
MinhKMA <nguyenvanminhkma@gmail.com>
2018-06-23 22:28:54 +0700

    update http check async request

--- thuoclao/check/tasks.py
+++ thuoclao/check/tasks.py
@@ -5,6 +5,9 @@ import asyncio
 import re
 from datetime import datetime
 from influxdb import InfluxDBClient
+from requests_futures.sessions import FuturesSession
+import time
+
 
 def get_fping():
     users = User.objects.all()
@@ -43,6 +46,8 @@ def get_http():
             item = {'id': host.id, 'hostname': host.hostname, 'status': host.status,
                 'description': host.description, 'group_name': host.group.group_name}
             item['interval_check'] = host.group.group_attribute_set.get(attribute_name='interval_check').value
+            url = host.host_attribute_set.get(attribute_name='url')
+            item['url'] = url.value
             hosts.append(item)
         data[user.username] = hosts
     # print(data)
@@ -54,7 +59,8 @@ fping_regex = re.compile(
     r"(.+=\s*(?P<min>[0-9.]+)/(?P<avg>[0-9.]+)/(?P<max>[0-9.]+))?")
 
 
-def write_influxdb(data, user, host_db=None, port=None, username=None,
+def write_influxdb(data, user, hostname, group_name,
+                   host_db=None, port=None, username=None,
                    password=None, database=None):
     host_db = host_db or '192.168.30.67'
     port = port or 8086
@@ -68,7 +74,9 @@ def write_influxdb(data, user, host_db=None, port=None, username=None,
             "measurement": "ping",
             "tags": {
                 "user": str(user),
-                "host": str(data.group("host"))
+                "ip": str(data.group("host")),
+                "group": str(group_name),
+                "hostname": str(hostname)
             },
             "fields": {
                 "sent": int(data.group("sent")) if data.group("sent") else 0,
@@ -83,49 +91,122 @@ def write_influxdb(data, user, host_db=None, port=None, username=None,
     client.write_points(json_body)
 
 
-async def custom_sleep(interval, stdout, stderr):
+async def custom_sleep(interval, user, hostname, group_name, stdout, stderr):
     await asyncio.sleep(interval)
+    print(type(interval))
     print(stdout, stderr)
     for line in stderr.decode().split('\n'):
         data = fping_regex.match(line)
         if data:
-            write_influxdb(data=data, user='minhkma')
+            write_influxdb(data=data, user= user, hostname= hostname,
+                           group_name= group_name)
     print('SLEEP {}\n'.format(datetime.now()))
 
 
-async def factorial(interval, *args):
+async def factorial(interval, user, hostname, group, *args):
     process = await asyncio.create_subprocess_shell(
         *args,
         stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await process.communicate()
-    await custom_sleep(interval, stdout, stderr)
+    await custom_sleep(interval, user, hostname, group, stdout, stderr)
 
 
-async def loop_exec(loop):
+async def loop_exec(loop, tasks):
     while loop.is_running():
-        tasks = [
-            asyncio.ensure_future(factorial(2, 'fping -c 4 dantri.vn')),
-            asyncio.ensure_future(factorial(3, 'fping -c 4 google.com')),
-        ]
+        # tasks = [
+        #     asyncio.ensure_future(factorial(2, 'fping -c 4 dantri.vn')),
+        #     asyncio.ensure_future(factorial(3, 'fping -c 4 google.com')),
+        # ]
         await asyncio.wait(tasks)
 
+session = FuturesSession()
 
-# async def loop_exec(loop, tasks):
-#     while loop.is_running():
-#         # tasks = [
-#         #     asyncio.ensure_future(factorial(2, 'fping -c 4 dantri.vn')),
-#         #     asyncio.ensure_future(factorial(3, 'fping -c 4 google.com')),
-#         # ]
-#         await asyncio.wait(tasks)
+def bg_cb(sess, resp, hostname, group_name, user,
+          host_db= None, port= None, username= None,
+                   password= None, database= None):
+    host_db = host_db or '192.168.30.67'
+    port = port or 8086
+    username = username or 'minhkma'
+    password = password or 'minhkma'
+    database = database or 'thuoclao'
+    client = InfluxDBClient(host=host_db, port=port, username=username,
+                            password=password, database=database)
+    json_body = [
+        {
+            "measurement": "http",
+            "tags": {
+                "url": str(resp.url),
+                'hostname': str(hostname),
+                'group': str(group_name),
+                'username': str(user)
+            },
+            "fields":
+                {
+                    "code": int(resp.status_code),
+                    "response": float(resp.elapsed.total_seconds())
+                }
+
+        }
+    ]
+    client.write_points(json_body)
+    # print("<---  {}".format(time.time()))
+
+
+async def http_exec(loop, url, interval, hostname, group_name, user):
+    future = session.get(url,
+                         background_callback=lambda sess,
+                                                    resp: bg_cb(sess,
+                                                                resp,
+                                                                hostname= hostname,
+                                                                group_name=group_name,
+                                                                user=user))
+    # response = future.result()
+    # pprint(response.data)
+    loop.call_later(int(interval), loop.create_task, http_exec(loop, url, int(interval),
+                                                          hostname, group_name,
+                                                          user))
+
+
+@background(schedule=0)
+def http():
+    data_http = get_http()
+    print(data_http)
+    loop = asyncio.get_event_loop()
+    for user in data_http:
+        for count, info_url in enumerate(data_http[user]):
+            url = info_url['url']
+            # print(url)
+            hostname = info_url['hostname']
+            group_name = info_url['group_name']
+            interval = int(info_url['interval_check'])
+            print(url, hostname, group_name, interval)
+            loop.call_soon(loop.create_task, http_exec(loop, url, int(interval),
+                                                      hostname, group_name,
+                                                       user))
+    loop.run_forever()
+http()
 
 @background(schedule=0)
 def fping():
+    tasks = []
     data_ping = get_fping()
-    data_http = get_http()
-    # loop = asyncio.get_event_loop()
-    # loop.create_task(loop_exec(loop, tasks))
-    # loop.create_task(loop_exec(loop))
-    # loop.run_forever()
     print(data_ping)
-    print(data_http)
+    for user in data_ping:
+        for count, info_ping in enumerate(data_ping[user]):
+            ip = info_ping['ip_address']
+            hostname = info_ping['hostname']
+            group_name = info_ping['group_name']
+            interval = int(info_ping['interval_ping'])
+            number_packet = info_ping['number_packet']
+            tasks.append(asyncio.ensure_future(factorial(interval,
+                                                         user,
+                                                         hostname,
+                                                         group_name,
+                                                         'fping -c {0} {1}'
+                                                         .format(number_packet,
+                                                                 ip)
+                                                         )))
+    loop = asyncio.get_event_loop()
+    loop.create_task(loop_exec(loop, tasks))
+    loop.run_forever()
 fping()
