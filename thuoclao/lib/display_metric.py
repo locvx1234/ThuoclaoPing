b7d6d75c34f570df6b73e528133bb4400b2faa49
MinhKMA <nguyenvanminhkma@gmail.com>
2018-06-26 10:17:05 +0700

    update select http and ping

--- thuoclao/lib/display_metric.py
+++ thuoclao/lib/display_metric.py
@@ -8,20 +8,46 @@ class Display(utils.Auth):
         self.user = user
         # self.range_time = range_time
         self.client = self.auth()
+    #
+    # def select(self, iterval_time_query):
+    #     # data = self.client.query('select * from fping where host=\'192.168.100.30\' and time > now() - 1m')
+    #     data = self.client.query('select * from {}'
+    #                              ' where \"host\" = \'{}\''
+    #                              ' and \"user\" = \'{}\' and time > now() - {}m'
+    #                              .format(self.service_name, self.host,
+    #                                      self.user, iterval_time_query), epoch='ms')
+    #     # print(data)
+    #     results = list(data.get_points(measurement='ping'))
+    #     # results = list(data.get_points(measurement='{}'
+    #     #                                .format(self.service_name)))
+    #     # print(results)
+    #     return results
 
-    def select(self, iterval_time_query):
-        # data = self.client.query('select * from fping where host=\'192.168.100.30\' and time > now() - 1m')
-        data = self.client.query('select * from {}'
-                                 ' where \"host\" = \'{}\''
-                                 ' and \"user\" = \'{}\' and time > now() - {}m'
-                                 .format(self.service_name, self.host,
-                                         self.user, iterval_time_query), epoch='ms')
-        # print(data)
-        results = list(data.get_points(measurement='ping'))
-        # results = list(data.get_points(measurement='{}'
-        #                                .format(self.service_name)))
-        # print(results)
-        return results
+    def select_http(self, hostname, group, url, username, interval):
+        data_http = self.client.query('select * from http'
+                                      ' where \"hostname\" = \'{}\' '
+                                      'and \"group\" = \'{}\' '
+                                      'and \"url\" = \'{}\' '
+                                      'and \"username\" = \'{}\' '
+                                      'and time > now() - {}m'
+                                      .format(hostname, group, url,
+                                              username, interval))
+        results_http = list(data_http.get_points(measurement='http'))
+        return results_http
+
+
+    def select_ping(self, hostname, group, url, username, interval):
+        data_ping = self.client.query('select * from ping'
+                                      ' where \"hostname\" = \'{}\' '
+                                      'and \"group\" = \'{}\' '
+                                      'and \"ip\" = \'{}\' '
+                                      'and \"username\" = \'{}\' '
+                                      'and time > now() - {}m'
+                                      .format(hostname, group, url,
+                                              username, interval))
+        results_ping = list(data_ping.get_points(measurement='ping'))
+        return results_ping
+
 
     def check_ping_notify(self, oke, warning, critical):
         data_status = self.client.query('select mean("loss") from ping '
