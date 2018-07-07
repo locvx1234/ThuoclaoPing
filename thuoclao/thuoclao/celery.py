5535cef6b3c09f6ca71499fcd75b4481b2a0b453
MinhKMA <nguyenvanminhkma@gmail.com>
2018-07-08 00:49:01 +0700

    config port 6379 of redis for celery

--- thuoclao/thuoclao/celery.py
+++ thuoclao/thuoclao/celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thuoclao.settings')
-app = Celery('thuoclao')
+app = Celery('thuoclao', broker='redis://localhost:6379/0')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
